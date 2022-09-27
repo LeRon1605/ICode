02171fbf01aae0573dc9b4beeb78eca28dd04d63
Ron Le <ronle9519@gmail.com>
2022-09-27 13:07:38 +0700

    Submission test

---
+++ UnitTest/Data/TestcaseData.cs
@@ -0,0 +1,26 @@
+﻿using CodeStudy.Models;
+using Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest.Data
+{
+    public static class TestcaseData
+    {
+        public static List<TestcaseDTO> GetAll()
+        {
+            return new List<TestcaseDTO>()
+            {
+                new TestcaseDTO
+                {
+                    ID = "1",
+                    Input = "1",
+                    Output = "1",
+                    MemoryLimit = 5000,
+                    TimeLimit = 0.001f
+                }
+            };
+        }
+    }
+}
